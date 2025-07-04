f24997e02f8f7ff71f572ed53a5aac4a690fd15a
viralbhoi <72viralbhoi@gmail.com>
2025-07-04 21:30:08 +0530

    added problem statement

--- Array/Hard/prob7.cpp
+++ Array/Hard/prob7.cpp
@@ -1,3 +1,7 @@
+// Merge Overlapping Sub-intervals
+// Problem Statement: Given an array of intervals, merge all the overlapping intervals and return an array of non-overlapping intervals.
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
